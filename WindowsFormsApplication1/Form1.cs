3f3197f1b56c1da1464985ee15bf001efd10dd8f
A1rPun <thombakker@gmail.com>
2016-11-13 18:41:06 +0100

    :beetle: Initial commit

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.ShowBalloonTip(10000, "Balloontip", "Please click on the notifyIcon below", ToolTipIcon.Info);
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("notifyIcon balloontip click");
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("notifyIcon click");
+        }
+    }
+}
